09be5d5ffefba087aed3361fa2584b31cf330b7c
annah-bot1 <masvikenigamuchirai@gmail.com>
2026-09-22 23:54:24 +0900

    Add files via upload
    week4 HW

---
+++ output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    int y;
+    scanf ("%d", &x);
+    scanf ("%d", &y);
+    printf("sum of the two numbers:%d\n", x+y);
+    printf("difference between the two numbers:%d\n", x-y);
+    printf("product of the two numbers:%d\n", x*y);
+    printf("quotient of the two numbers:%d\n", x/y);
+    return 0;
+}
